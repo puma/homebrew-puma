90d661fe42895600f38f71dc0a182d9337cc88ce
Alan Norton <git@nonration.al>
2021-01-14 15:08:43 -0500

    v0.15.2 (#11)

--- puma-dev.rb
+++ puma-dev.rb
@@ -1,9 +1,9 @@
 class PumaDev < Formula
   desc "A tool to manage rack apps in development with puma"
   homepage "https://github.com/puma/puma-dev"
-  url "https://github.com/puma/puma-dev/releases/download/v0.15.1/puma-dev-0.15.1-darwin-amd64.zip"
-  sha256 "a9348e67431ee65f6f41e2ad758d5c4212996d2c2c92e51229a412bd342c7864"
-  version '0.15.1'
+  url "https://github.com/puma/puma-dev/releases/download/v0.15.2/puma-dev-0.15.2-darwin-amd64.zip"
+  sha256 "9b8efd851283f1ddb284580ac200c233ab9b285d770752d51d6d338f7c2020ab"
+  version '0.15.2'
 
   def install
     bin.install "puma-dev"
