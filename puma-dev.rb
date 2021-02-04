f4f70d33158dc508e1fcb5077e11b1234becb72e
Alexander ADAM <alexanderadam@users.noreply.github.com>
2021-02-04 14:39:02 +0100

    enable GNU/Linux support 🐧 (#12)
    fixes #4

--- puma-dev.rb
+++ puma-dev.rb
@@ -1,12 +1,20 @@
 class PumaDev < Formula
-  desc "A tool to manage rack apps in development with puma"
-  homepage "https://github.com/puma/puma-dev"
-  url "https://github.com/puma/puma-dev/releases/download/v0.15.2/puma-dev-0.15.2-darwin-amd64.zip"
-  sha256 "9b8efd851283f1ddb284580ac200c233ab9b285d770752d51d6d338f7c2020ab"
+  desc 'A tool to manage rack apps in development with puma'
+  homepage 'https://github.com/puma/puma-dev'
   version '0.15.2'
 
+  base_url = "https://github.com/puma/puma-dev/releases/download/v#{version}/puma-dev-#{version}"
+
+  if OS.mac?
+    url "#{base_url}-darwin-amd64.zip"
+    sha256 '9b8efd851283f1ddb284580ac200c233ab9b285d770752d51d6d338f7c2020ab'
+  elsif OS.linux?
+    url "#{base_url}-linux-amd64.tar.gz"
+    sha256 '89995e01e07f30d5e41b2d9060c837a521228b0009a0ab037ee7ac066cc2403c'
+  end
+
   def install
-    bin.install "puma-dev"
+    bin.install 'puma-dev'
   end
 
   test do
@@ -18,13 +26,23 @@ class PumaDev < Formula
   end
 
   def caveats
-    <<~EOS
+    if OS.mac?
+      <<~EOS
       Setup dev domains:
         sudo puma-dev -setup
 
       Install puma-dev as a launchd agent (required for port 80 usage):
         puma-dev -install
-    EOS
+      EOS
+    elsif OS.linux?
+      <<~EOS
+        You may want to have a look at
+
+          https://github.com/puma/puma-dev#linux-support
+
+        for more information on how to optimize puma-dev (i.e. port 80 support & root certificate)
+      EOS
+    end
   end
 end
 
