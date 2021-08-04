fb345f287ee5d0fd52841d48fd4f9df438026bd8
Alan Norton <git@nonration.al>
2021-08-03 20:46:15 -0400

    v0.16.2  (#16)

--- puma-dev.rb
+++ puma-dev.rb
@@ -1,16 +1,19 @@
 class PumaDev < Formula
   desc 'A tool to manage rack apps in development with puma'
   homepage 'https://github.com/puma/puma-dev'
-  version '0.16.1'
+  version '0.16.2'
 
   base_url = "https://github.com/puma/puma-dev/releases/download/v#{version}/puma-dev-#{version}"
 
-  if OS.mac?
+  if OS.mac? and Hardware::CPU.arm?
+    url "#{base_url}-darwin-arm64.zip"
+    sha256 'fc22a1aed4f31f9c6ee96750db5f1dcbeb0cf5171c7021a9963a08f48d7252aa'
+  elsif OS.mac?
     url "#{base_url}-darwin-amd64.zip"
-    sha256 'cd399611ee350952b24ad8555d731253510e03fdd821b09b5ae7135faa24778c'
+    sha256 '2321ea4a60c7e3ef8c0723258476a26efc4732cbda06e4fed61055638817d819'
   elsif OS.linux?
     url "#{base_url}-linux-amd64.tar.gz"
-    sha256 'cbd1f5266b1a198d60b5fc26fc605abc938efc00fa90ceb1c36701e69272f72f'
+    sha256 'ae99a26beece5f7b311dbdc06f260bf1ca23a412788d88d1b62d2e810c7c9757'
   end
 
   def install
@@ -45,4 +48,3 @@ class PumaDev < Formula
     end
   end
 end
-
