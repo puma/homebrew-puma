5960519de5e559a6326626643c0a7e4b2fd9832d
Alan Norton <git@nonration.al>
2023-02-25 15:29:17 -0500

    v0.18.3 (#18)
    * need to setup homebrew on linux now
    
    * v0.18.3

--- puma-dev.rb
+++ puma-dev.rb
@@ -1,19 +1,19 @@
 class PumaDev < Formula
   desc 'A tool to manage rack apps in development with puma'
   homepage 'https://github.com/puma/puma-dev'
-  version '0.18.2'
+  version '0.18.3'
 
   base_url = "https://github.com/puma/puma-dev/releases/download/v#{version}/puma-dev-#{version}"
 
   if OS.mac? and Hardware::CPU.arm?
     url "#{base_url}-darwin-arm64.zip"
-    sha256 '624b8dbe99e60905419ad659c129a4bfbf937383104821c117bb4926be2ca511'
+    sha256 '3df8b238ae8318d2591cbd69b372e304d6f8f39455d1a298420f53a31ad50cc7'
   elsif OS.mac?
     url "#{base_url}-darwin-amd64.zip"
-    sha256 '7db1442c073b21de797e4e9d353b7b8427cfea6df68ac6b146912db1ca799d05'
+    sha256 '413c73504bbf4ac150cb76dd7433d86aa76b0f3bdd76d9ff2e0c3a29927557ae'
   elsif OS.linux?
     url "#{base_url}-linux-amd64.tar.gz"
-    sha256 '23473eae5b4f5610fa289c601a59aecbd7fd3e4d1d1864acee099400b3279dd5'
+    sha256 'f817a38fb7138e0008923f994612bded0e6e55e69755f08be0b661b896b47ad2'
   end
 
   def install
